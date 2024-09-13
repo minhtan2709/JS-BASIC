92aa4243cbccc78a1638508da6f3af873c6cf974
minhtan2709 <tantran2709vn@gmail.com>
2024-09-13 21:42:38 +0700

    Map in JS

---
+++ Map/map.js
@@ -0,0 +1,15 @@
+console.log("Hello World with HTML")
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+// for(let i =0;i<arr.length;i++){
+//     arr[i]= arr[i] * arr[i];
+//     console.log(arr[i])
+// }
+let maparr = arr.map((item,index)=>{
+    item =item * item;
+    return item;
+})
+console.log("Check value: ",arr)
+console.log("Check value: ",mapparr)
+
+
